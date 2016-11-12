ebe4b8ec7d163c174a66b6f206b5feaadc65d22b
Bernardo Vieira <bernardo5@live.com.pt>
2016-11-12 22:54:08 +0000

    CountdownAndVerification
    - Countdown working and verification - Finish window prepared!

---
+++ app/src/main/java/a21240068/isec/nerdquiz/FinishGameActivity.java
@@ -0,0 +1,24 @@
+package a21240068.isec.nerdquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FinishGameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish_game);
+    }
+
+    public void clickButtonPlayAgain(View view)
+    {
+        //
+    }
+
+    public void clickButtonGoBack(View view)
+    {
+        //
+    }
+}
