d446781f7585a61ccad69bfc40078060f8a37ec4
Bernardo Vieira <bernardo5@live.com.pt>
2016-12-13 18:38:41 +0000

    TakePhoto

--- nerd.quiz/app/src/main/java/a21240068/isec/nerdquiz/MainActivity.java
+++ nerd.quiz/app/src/main/java/a21240068/isec/nerdquiz/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends Activity {
 
         if(username.equals(defaultValue))
         {
-            Intent intent = new Intent(this, RegisterActivity.class);
+            Intent intent = new Intent(this, TakePhotoActivity.class);
             startActivity(intent);
         }
         else
